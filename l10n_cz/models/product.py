82a6682dad5e4e7b47afbfdb8a6a42202eecf0b1
Amevia <66975640+Amevia-sro@users.noreply.github.com>
2021-10-24 16:53:31 +0200

    Delete l10n_cz directory

--- l10n_cz/models/product.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    travel_service = fields.Boolean('Travel Service')
-    used_goods = fields.Boolean('Used Goods')
